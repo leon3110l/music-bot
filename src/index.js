2f06e02f03f360b71a88f17e193e86cde41d746a
leon in 't Veld <leon3110l@gmail.com>
2017-11-02 22:18:43 +0100

    added an spotify link parser thing and added playlist and album with yt search

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ import {
   spotify_secret,
   spotify_id,
   youtube_token,
-} from './conf.json'
+  yt_url,
+} from './conf'
+import help from './help'
 import MessageHandler from './messageHandler'
+import SpotifyParser from './spotifyParser'
 
 const bot = new Discord.Client()
 const handler = new MessageHandler(prefix)
@@ -17,6 +20,7 @@ var spotifyApi = new SpotifyWebApi({
   clientId: spotify_id,
   clientSecret: spotify_secret,
 })
+const parser = new SpotifyParser()
 
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant().then(
@@ -36,6 +40,16 @@ bot.on('ready', () => {
   bot.generateInvite(['SEND_MESSAGES', 'MENTION_EVERYONE']).then(link => {
     console.log(`Generated bot invite link: ${link}`)
   })
+  bot.user.setGame(prefix + 'help for help')
+})
+
+handler.set('help', message => {
+  let msg = 'help: \r\n\r\n'
+  console.log(help)
+  for (let key in help) {
+    msg += `${prefix}${key}: ${help[key]}\r\n\r\n`
+  }
+  message.author.send(msg)
 })
 
 handler.set('invite', message => {
@@ -65,7 +79,7 @@ handler.set('yt:multiple', (message, parts) => {
       const { items } = res
       let msg = 'found\r\n\r\n'
       items.forEach((x, i) => {
-        msg += `${i + 1}. https://www.youtube.com/watch?v=${x.id.videoId}\r\n`
+        msg += `${i + 1}. ${yt_url}${x.id.videoId}\r\n`
       })
       msg += ''
       message.reply(msg)
@@ -74,19 +88,98 @@ handler.set('yt:multiple', (message, parts) => {
 })
 
 handler.set('yt', (message, parts) => {
-  if (parts[1]) {
-    const q = parts.slice(1).join(' ')
-    ytsearch({ q }).then(res => {
-      const { items } = res
-      let msg = 'found\r\n\r\n'
-      for (let x of items.filter(musicFilter(q))) {
-        msg += `https://www.youtube.com/watch?v=${x.id.videoId}\r\n`
-        break
-      }
-      message.reply(msg)
-    })
-  }
+  if (!parts[1]) return
+
+  const q = parts.slice(1).join(' ')
+  ytsearch({ q }).then(res => {
+    const { items } = res
+    let msg = 'found\r\n\r\n'
+    for (let x of items.filter(musicFilter(q))) {
+      msg += `${yt_url}${x.id.videoId}\r\n`
+      break
+    }
+    message.reply(msg)
+  })
 })
+
+const album = (message, parts) => {
+  if (!parts[0]) return
+
+  spotifyApi
+    .getAlbums(
+      parts
+        .slice(1)
+        .map(x => parser.parse(x))
+        .map(x => x.album),
+    )
+    .then(data => {
+      const { albums } = data.body
+      albums.forEach(x => {
+        const { items } = x.tracks
+        let msg = 'got: \r\n'
+
+        const all = []
+        const qs = []
+
+        items.forEach((x, i) => {
+          const q = x.artists[0].name + ' - ' + x.name
+          qs.push(q)
+          all.push(ytsearch({ q }))
+        })
+
+        Promise.all(all).then(a => {
+          a.forEach((res, i) => {
+            const { items } = res
+            for (let x of items.filter(musicFilter(qs[i]))) {
+              msg += `${qs[i]}:\r\n${yt_url}${x.id.videoId}\r\n`
+              break
+            }
+            console.log(i, all.length)
+            if ((i % 5 === 0 && i != 0) || i === all.length - 1) {
+              message.reply(msg)
+              msg = ''
+            }
+          })
+        })
+      })
+    })
+}
+handler.set('albums', album)
+handler.set('album', album)
+
+const playlist = (message, parts) => {
+  parts.slice(1).forEach(x => {
+    const parsed = parser.parse(x)
+    console.log(parsed)
+    spotifyApi.getPlaylist(parsed.user, parsed.playlist).then(res => {
+      const { items } = res.body.tracks
+      let msg = 'got: \r\n'
+      const all = []
+      const qs = []
+      items.forEach((x, i) => {
+        x = x.track.album
+        const q = `${x.artists[0].name} - ${x.name}`
+        all.push(ytsearch({ q }))
+        qs.push(q)
+      })
+      Promise.all(all).then(a => {
+        a.forEach((res, i) => {
+          const { items } = res
+          for (let x of items.filter(musicFilter(qs[i]))) {
+            msg += `${qs[i]}:\r\n${yt_url}${x.id.videoId}\r\n`
+            break
+          }
+          if ((i % 5 === 0 && i != 0) || i === all.length - 1) {
+            message.reply(msg)
+            msg = ''
+          }
+        })
+      })
+    })
+  })
+}
+handler.set('playlist', playlist)
+handler.set('playlists', playlist)
 
 bot.on('message', message => {
   handler.handle(message)
@@ -116,7 +209,7 @@ function ytsearch(opt) {
 const musicFilter = q => x =>
   x.snippet.channelTitle.toLowerCase().search(
     new RegExp(
-      `vevo|${q
+      `vevo|official|audio|lyric|${q
         .toLowerCase()
         .split(' ')
         .join('|')}`,
---
+++ src/spotifyParser.js
@@ -0,0 +1,21 @@
+// spotify:album:0BFzNaeaNv4mahOzwZFGHK
+// spotify:track:1INksmStBpxdNlBzwGUvsg
+// spotify:artist:2S5hlvw4CMtMGswFtfdK15
+// spotify:user:leon3110l:playlist:10PQQ2oyZ9y8iQTciz479z
+
+export default class SpotifyParser {
+  parse = x => {
+    const output = {}
+    x = x
+      .split('spotify:')
+      .slice(1)
+      .join('')
+      .split(':')
+
+    x.forEach((y, i) => {
+      i % 2 === 0 ? (output[y] = x[i + 1]) : null
+    })
+
+    return output
+  }
+}
