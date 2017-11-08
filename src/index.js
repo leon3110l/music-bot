49876cc8fe79c44b65b596770e5feb491992066b
leon in 't Veld <leon3110l@gmail.com>
2017-11-08 12:14:01 +0100

    add refresh token stuff

--- src/index.js
+++ src/index.js
@@ -31,6 +31,17 @@ spotifyApi.clientCredentialsGrant().then(
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token'])
     spotifyApi.setRefreshToken(data.body['refresh_token'])
+    setInterval(() => {
+      spotifyApi.refreshAccessToken()
+      .then((data) => {
+        console.log('The access token has been refreshed!');
+
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+      }, (err) => {
+        console.log('Could not refresh access token', err);
+      });
+    }, 3550000)
   },
   err => {
     console.log('Something went wrong when retrieving an access token', err)
